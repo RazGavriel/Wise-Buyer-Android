c318aeb3aac5d90669fefb252007c89996bf3eb1
Raz Hagay Gavriel <raz5338@gmail.com>
2024-01-02 20:38:01 +0200

    implement firebase in login fragment

--- app/src/main/java/com/app/wisebuyer/login/LoginFragment.kt
+++ app/src/main/java/com/app/wisebuyer/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import com.app.wisebuyer.R
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,34 +18,61 @@ import androidx.navigation.fragment.findNavController
 class LoginFragment : Fragment() {
 
     private val loginViewModel: LoginViewModel by activityViewModels()
-
+    private lateinit var emailInput : EditText
+    private lateinit var passwordInput : EditText
+    private lateinit var loginButton: Button
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(
             R.layout.fragment_login, container, false
         )
-        val usernameInput = view.findViewById<EditText>(R.id.username_input)
-        val passwordInput = view.findViewById<EditText>(R.id.password_input)
+        emailInput = view.findViewById<EditText>(R.id.email_input)
+        passwordInput = view.findViewById<EditText>(R.id.password_input)
+        loginButton = view.findViewById<Button>(R.id.login_button)
 
-        val button:Button = view.findViewById<Button>(R.id.login_button)
-
-        button.setOnClickListener {
-            Log.v("APP", "Login button clicked")
-            val credentials = UserCredentials(usernameInput.text.toString(), passwordInput.text.toString())
-            loginViewModel.loginUser(credentials)
-        }
+        handleButtonClick(loginButton)
         // Handle the result here
-        // For example, update UI based on the login result
-        // result is the value emitted by the LiveData
+        observeLoginResult()
+        return view
+    }
+
+    private fun observeLoginResult() {
         loginViewModel.loginResult.observe(viewLifecycleOwner) { result: Boolean ->
             Log.v("APP", "login result: $result")
-            if(result){
+            if (result) {
                 findNavController().navigate(R.id.action_loginFragment_to_postsFragment)
             }
         }
-        return view
+    }
+
+    private fun handleButtonClick(loginButton:Button) {
+        loginButton.setOnClickListener {
+            Log.v("APP", "Login button clicked")
+            val credentials =
+                UserCredentials(emailInput.text.toString(), passwordInput.text.toString())
+            if (checkAllFields(credentials)) {
+                loginViewModel.loginUser(credentials)
+            }
+        }
+    }
+
+    private fun checkAllFields(credentials: UserCredentials): Boolean{
+        if (credentials.email.isEmpty()){
+            emailInput.error = "Enter a email"
+        }
+        else if (credentials.password.isEmpty()){
+            passwordInput.error = "Enter a password"
+        }
+        else if (credentials.password.length <=6) {
+            passwordInput.error = "Password need to more than 6 characters long"
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(credentials.email).matches()){
+            emailInput.error = "Enter valid email format"
+        }
+        else{ return true }
+        return false
     }
 }
--- app/src/main/java/com/app/wisebuyer/login/LoginModel.kt
+++ app/src/main/java/com/app/wisebuyer/login/LoginModel.kt
@@ -1,3 +1,3 @@
 package com.app.wisebuyer.login
 
-data class UserCredentials(val username: String, val password: String)
+data class UserCredentials(val email: String, val password: String)
