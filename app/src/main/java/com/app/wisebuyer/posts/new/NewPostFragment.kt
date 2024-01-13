42a736fc2da030078203a0b86e69300e360b78cc
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-01-13 18:08:27 +0200

    add new post fragment + UI + change theme

--- app/src/main/java/com/app/wisebuyer/posts/new/NewPostFragment.kt
+++ app/src/main/java/com/app/wisebuyer/posts/new/NewPostFragment.kt
@@ -1,24 +1,22 @@
-package com.app.wisebuyer.posts
+package com.app.wisebuyer.posts.new
 
-import com.app.wisebuyer.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.app.wisebuyer.R
 
-class PostsFragment : Fragment() {
-//    private lateinit var searchInput: EditText
-
+class NewPostFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(
-            R.layout.fragment_posts, container, false
+            R.layout.fragment_new_post, container, false
         )
-//        searchInput = view.findViewById<EditText>(R.id.search_input)
+
         return view
     }
 }
