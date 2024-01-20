a9f749d86b3a24ac6670e9f21f3bd35d5ab95ab6
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-01-20 09:47:27 +0200

    load product image from firestorage

--- app/src/main/java/com/app/wisebuyer/posts/PostCardsAdapter.kt
+++ app/src/main/java/com/app/wisebuyer/posts/PostCardsAdapter.kt
@@ -1,5 +1,6 @@
 package com.app.wisebuyer.posts
 
+import android.util.Log
 import com.app.wisebuyer.R
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 
 class PostCardsAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostCardsAdapter.PostViewHolder>() {
+
+    private val storage = FirebaseStorage.getInstance()
     class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val image: ImageView = itemView.findViewById(R.id.card_image)
         val title: TextView = itemView.findViewById(R.id.card_title)
@@ -24,6 +29,11 @@ class PostCardsAdapter(private val posts: List<Post>) : RecyclerView.Adapter<Pos
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         // TODO: Add more logic here: thumbs up / down for post etc
         val post = posts[position]
+        storage.reference.child(post.productPicture).downloadUrl.addOnSuccessListener {
+            Glide.with(holder.itemView)
+                .load(it)
+                .into(holder.image);
+        }
         holder.title.text = post.title
         holder.description.text = post.description
         holder.link.text = post.link
