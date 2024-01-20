338809a2a2ff3e6ddf344186742aa70f412b3626
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-01-20 09:00:30 +0200

    add recycler view

--- app/src/main/java/com/app/wisebuyer/posts/PostsFragment.kt
+++ app/src/main/java/com/app/wisebuyer/posts/PostsFragment.kt
@@ -9,13 +9,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.app.wisebuyer.utils.RequestStatus
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PostsFragment : Fragment() {
 
     private val postViewModel: PostViewModel by activityViewModels()
     private lateinit var addNewPostButton: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +26,8 @@ class PostsFragment : Fragment() {
             R.layout.fragment_posts, container, false
         )
         addNewPostButton = view.findViewById<FloatingActionButton>(R.id.add_new_post_button)
+        recyclerView = view.findViewById<RecyclerView>(R.id.posts_recycler_view)
+
         addNewPostButton.setOnClickListener {
             findNavController().navigate(R.id.action_postsFragment_to_newPostFragment)
         }
