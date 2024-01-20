75ec8cdd437895d63151ced4ac85ef0fc46586fc
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-01-20 09:57:25 +0200

    fix serilaze error

--- app/src/main/java/com/app/wisebuyer/posts/PostModel.kt
+++ app/src/main/java/com/app/wisebuyer/posts/PostModel.kt
@@ -40,5 +40,5 @@ data class Post(
     val price: String = "",
     var productPicture: String = "",
     var userEmail: String = "",
-    var createdAt: Number = Date().time
+    var createdAt: Long = Date().time
 )
