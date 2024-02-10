9d382e39aa1f47f60432812462700572a84b4931
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-10 18:25:33 +0200

    move showDialogResponse to global function to reduce double code

--- app/src/main/java/com/app/wisebuyer/utils/globalFunctions.kt
+++ app/src/main/java/com/app/wisebuyer/utils/globalFunctions.kt
@@ -3,6 +3,8 @@ package com.app.wisebuyer.utils
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 fun manageViews(vararg views: View, mode: String) {
     for (view in views) {
@@ -14,4 +16,13 @@ fun manageViews(vararg views: View, mode: String) {
 fun closeKeyboard(context: Context, view:View) {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun showDialogResponse(message: String, rootView: View) {
+    val snackBar = Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT)
+    val snackBarView: View = snackBar.view
+    snackBarView.setBackgroundColor(0xFF000000.toInt()) // black color
+    val textView: TextView = snackBarView.findViewById(com.google.android.material.R.id.snackbar_text)
+    textView.setTextColor(0xFFFFFFFF.toInt()) // white color
+    snackBar.show()
 }
