cfe314256ed3888c0c30668768a397eccbd4bdcb
Raz-Hagay-Gavriel <raz5338@gmail.com>
2024-02-10 18:29:45 +0200

    use show showDialogResponse from validationFunctions.kt file

--- app/src/main/java/com/app/wisebuyer/shared/PostBaseFragment.kt
+++ app/src/main/java/com/app/wisebuyer/shared/PostBaseFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -23,7 +22,7 @@ import com.app.wisebuyer.singup.UserProperties
 import com.app.wisebuyer.utils.RequestStatus
 import com.app.wisebuyer.utils.closeKeyboard
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
+import com.app.wisebuyer.utils.showDialogResponse
 
 abstract class PostBaseFragment : Fragment(), PostCardsAdapter.OnPostItemClickListener {
     lateinit var sharedViewModel: SharedViewModel
@@ -59,7 +58,7 @@ abstract class PostBaseFragment : Fragment(), PostCardsAdapter.OnPostItemClickLi
         postViewModel.requestStatus.observe(viewLifecycleOwner) { result ->
             if (result == RequestStatus.FAILURE) {
                 showDialogResponse("Something went wrong while processing your request. " +
-                        "Please try again later.")
+                        "Please try again later.", requireView())
             }
         }
     }
@@ -70,7 +69,7 @@ abstract class PostBaseFragment : Fragment(), PostCardsAdapter.OnPostItemClickLi
                 handleUIAfterLike(result)
             } else {
                 showDialogResponse("Something went wrong while processing your request. " +
-                        "Please try again later.")
+                        "Please try again later.", requireView())
             }
         }
     }
@@ -108,16 +107,6 @@ abstract class PostBaseFragment : Fragment(), PostCardsAdapter.OnPostItemClickLi
         if (result.postEmail == sharedViewModel.userMetaData.email){
             result.holder.deleteCardButton.visibility = View.VISIBLE }
         else{ result.holder.deleteCardButton.visibility = View.GONE }
-    }
-
-    open fun showDialogResponse(message: String) {
-        val rootView: View = requireView()
-        val snackBar = Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT)
-        val snackBarView: View = snackBar.view
-        snackBarView.setBackgroundColor(resources.getColor(R.color.black))
-        val textView: TextView = snackBarView.findViewById(com.google.android.material.R.id.snackbar_text)
-        textView.setTextColor(resources.getColor(R.color.white)) // Set your desired text color
-        snackBar.show()
     }
 
     override fun onPostItemClicked(postId: String, postEmail: String,
