6fd13d5013b330776320fbf5b9f36a49f34f5b55
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-02-10 19:05:15 +0200

    change room posts query to upsert

--- app/src/main/java/com/app/wisebuyer/room/PostDao.kt
+++ app/src/main/java/com/app/wisebuyer/room/PostDao.kt
@@ -1,6 +1,7 @@
 package com.app.wisebuyer.room
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.app.wisebuyer.posts.Post
 
@@ -8,7 +9,10 @@ import com.app.wisebuyer.posts.Post
 interface PostDao {
     @Query("SELECT * FROM post")
     fun getAll(): List<Post>
-    @Insert
-    fun insertAll(vararg post: Post)
+
+    @Query("SELECT * FROM post WHERE userEmail = :email")
+    fun getAllPostByUserEmail(email: String): List<Post>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(vararg post: Post)
 
 }
